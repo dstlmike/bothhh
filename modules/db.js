9223a45fa36dbaaa9add12d10f957c62c83d5d03
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-10 18:55:03 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,7 +1,7 @@
 var mongoDB     = require('mongodb').MongoClient;
 
 //var connection_string = 'mongodb://127.0.0.1:27017/nodejs';
-var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
