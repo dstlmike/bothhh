732d86c89a24d4a19e4a6cef1f8c9f92cd10b9d5
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-10 20:11:06 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -10,7 +10,7 @@ var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha
 //  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
 //  process.env.OPENSHIFT_APP_NAME;
 //}
-
+/*
 if(process.env.MONGODB_PASSWORD){
 
   // //connection_string = 'mongodb://dstl%5Fmike1%40hotmail%2Ecom:308boonave@cluster0-shard-00-00-esmha.mongodb.net:27017,cluster0-shard-00-01-esmha.mongodb.net:27017,cluster0-shard-00-02-esmha.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' + process.env.MONGODB_USER + ":" +
@@ -26,7 +26,7 @@ if(process.env.MONGODB_PASSWORD){
   process.env.MONGODB_DATABASE;
 
 }
-
+*/
 function connect(callback){
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
