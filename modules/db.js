eec908aa07301e56751c11d7fe86a29fd0c1b799
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-11 04:27:49 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -6,7 +6,7 @@ var mongodbOptions = {
 useNewUrlParser: true,
 useUnifiedTopology: true
 }
-getAllDocuments();
+//getAllDocuments();
 /*
 mongoDB.connect(connection_string, function(err, client) {
 
@@ -56,7 +56,7 @@ function connect(callback){
   });
 }
 
-function getAllDocuments(collection, callback) {
+/*function getAllDocuments(collection, callback) {
 
   connect(function(db){
     var cursor = db.collection('sampledb3').find();
@@ -71,7 +71,7 @@ function getAllDocuments(collection, callback) {
 
 }
 
-
+*/
 
 exports.getAllDocuments = function(collection, callback) {
   mongoDB.connect("mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
