e95270c576d2e45b6f86507334fa112bfe29536d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-11 03:21:43 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -56,9 +56,9 @@ function connect(callback){
   });
 }
 
-function getAllDocuments(db, collection, callback) {
+function getAllDocuments(collection, callback) {
 
-  mongoDB.connect("mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, docs) {
+  mongoDB.connect("mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
 
     if(err) throw err;
 
