e9191fd844eef3d28dadf802c00b5e89b77d15d6
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-10 20:28:56 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -29,7 +29,7 @@ if(process.env.MONGODB_PASSWORD){
 //  process.env.MONGODB_DATABASE;
 
 }
-*/
+
 function connect(callback){
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
