43910d2a1efab56a1d551cb4f4ada4fb1c8cf768
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-11 03:52:32 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -58,7 +58,7 @@ function connect(callback){
 
 function getAllDocuments(collection, callback) {
 
-  mongoDB.connect("mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
+  connect(function(db) { //"mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
 
   //  if(err) throw err;
 
@@ -67,7 +67,7 @@ function getAllDocuments(collection, callback) {
    // var allDocs = 
         db.collection(collection).find().toArray(function(err, docs) {
 //allDocs;
-          if(callback)
+         // if(callback)
       callback(docs);
 
       db.close();
