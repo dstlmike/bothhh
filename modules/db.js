e486511ef58d7312d20ebf7fd2e7ab6d3e598807
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 06:42:22 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -6,8 +6,8 @@ var mongodbOptions = {
 useNewUrlParser: true,
 useUnifiedTopology: true
 }
-getAllDocuments();
-function getAllDocuments(res) {
+//getAllDocuments();
+//function getAllDocuments(res) {
 mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
 //rooms = {};
 // if (err) throw err
@@ -24,7 +24,7 @@ mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.ne
   });
 
 });
-  }
+//  }
 
 
 //run().catch(console.dir);
