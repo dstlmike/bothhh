f1115c26987741c9dea14654fea5997ec01642e5
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 06:13:49 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -7,7 +7,7 @@ useNewUrlParser: true,
 useUnifiedTopology: true
 }
 getAllDocuments();
-function getAllDocuments(collection, callback) {
+function getAllDocuments(collection, db, callback) {
 mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(db) {
 
 // if (err) throw err
