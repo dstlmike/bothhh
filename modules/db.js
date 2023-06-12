caa586bf53b108660800473ad2a62f5811efd5eb
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 02:00:43 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -13,7 +13,7 @@ var MongoClient = require('mongodb').MongoClient;
 var uri = "mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 async function listCars() {
 
-    let db = await MongoClient.connect(url);
+    let db = await MongoClient.connect(uri);
 
     let dbo = db.db("sampledb");
 
