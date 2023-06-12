dedb7f872cbe789c15912bd01190f5486dda60cb
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-11 23:35:23 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -11,7 +11,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 var uri = "mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-MongoClient.connect(uri, function(err, client, callback) {
+MongoClient.connect(uri, function(err, client) {
 
   const cursor = client.db("sampledb").collection("config").find({});
 //collection.find().each(function(err, results) {
@@ -25,10 +25,10 @@ var ret = [];
 
       //else
 
-        callback(ret);
+        //callback(ret);
   
   //results = [];
-      console.log(ret);
+      console.log(results);
 
     //  db.close();
   // perform actions on the collection object
