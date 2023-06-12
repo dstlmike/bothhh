52bd9adb9d4cf208001e79d789368faf0d60de6a
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 03:18:59 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -17,7 +17,7 @@ async function listCars() {
 
     let dbo = db.db("sampledb");
 
-    return await dbo.collection("config").find({});
+    return await dbo.collection("config").find({"config": 1});
 //results;
 //});
 };
