4266d7c6b2d3c23c5630c0616f0b74fcea21b3ba
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-11 23:06:19 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -6,6 +6,23 @@ var mongodbOptions = {
 useNewUrlParser: true,
 useUnifiedTopology: true
 }
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+MongoClient.connect(uri, function(err, client) {
+
+  const collection = client.db("test").collection("devices");
+
+  // perform actions on the collection object
+
+  client.close();
+
+});
+
+
+
 //
 /*const {MongoClient} = require('mongodb');
 main().catch(console.error);
