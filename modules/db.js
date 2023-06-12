f74a69dc443b450e2ef82c97726b9dd4fc8e500f
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 06:29:49 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -7,21 +7,21 @@ useNewUrlParser: true,
 useUnifiedTopology: true
 }
 getAllDocuments();
-function getAllDocuments(db, rooms, callback) {
-mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(db) {
-rooms = {};
+function getAllDocuments(res) {
+mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
+//rooms = {};
 // if (err) throw err
 
 //  var dbo = db.collection("sampledb3");
 
-  db.rooms.find({"name": 1}).toArray(function(err, result) {
+  db.collection("rooms").find({"name": 1}).toArray(function(err, result) {
 
    // if (err) throw err;
 //callback(docs);
-    
+    console.log('Results!!! ' + result);
 
     db.close();
-console.log('Results!!! ' + result);
+
   });
 
 });
