06759cfca2ebe898212a70399697109a6518e8c7
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 05:50:57 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -12,13 +12,13 @@ connect(function(db) {
 
 // if (err) throw err
 
-  var dbo = db.db("sampledb");
+  var dbo = db.db("sampledb3");
 
-  dbo.collection("rooms").find({"name": 1}).toArray(function(err, results) {
+  dbo.collection("rooms").find({"name": 1}).toArray(function(err, result) {
 
    // if (err) throw err;
 //callback(docs);
-    console.log('Results!!! ' + results);
+    console.log('Results!!! ' + result);
 
     db.close();
 
@@ -270,7 +270,7 @@ mongoDB.connect(connection_string, function(err, client) {
 
 
 function connect(callback){
-  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
+  mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
     callback(db);
   });
