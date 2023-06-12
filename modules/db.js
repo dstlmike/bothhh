5226421389344035fd9ec104ca9afeaeb5b4da0e
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 06:12:26 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -8,7 +8,7 @@ useUnifiedTopology: true
 }
 getAllDocuments();
 function getAllDocuments(collection, callback) {
-mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";", function(db) {
+mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(db) {
 
 // if (err) throw err
 
