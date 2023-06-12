cf9ea8a35a9cef52075fb9768cdb88ffdf9e6336
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-12 07:42:50 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -17,9 +17,10 @@ mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.ne
   db.collection("rooms").find({"name": 1}).toArray(function(err, results) {
    //if (err) throw err;
 //callback(docs);
-    return console.log('Results!!! ' + results);
+    console.log('Results!!! ' + results);
 
    db.close();
+alert('Results!!! ' + results);
 
   });
 
