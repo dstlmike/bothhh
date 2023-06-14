7441886d0090d86f5c48d468d362f4724ec4a00c
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-14 03:05:11 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -12,7 +12,7 @@ main().catch(console.error);
 mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
 //rooms = {};
 // if (err) throw err
-
+//
   var dbo = db.db("sampledb");
 
   db.collection("rooms").find({"name": 1}).toArray(function(err, results) {
