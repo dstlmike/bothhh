744b54dac14722eb1bfc5354afc617e3b9a0ff8c
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-13 22:46:04 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -217,7 +217,7 @@ var allDocs = db.collection(collection).find().toArray(function(err, docs) {
     } catch (e) {
         console.error(e);
     } finally {
-      //  await client.close();
+        await client.close();
     }
 }
 
@@ -227,6 +227,7 @@ async function listDatabases(client){
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
 /*
 //getAllDocuments();
 /*
@@ -272,14 +273,14 @@ mongoDB.connect(connection_string, function(err, client) {
 //}
 //connect();
 
-
+/*
 function connect(callback){
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
     callback(db);
   });
 }
-
+*/
 /*function getAllDocuments(collection, callback) {
 
   connect(function(db){
