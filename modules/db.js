45b8b67ee3669f99decbe046d8b6da91b48054dd
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-14 04:01:48 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -315,7 +315,7 @@ exports.getAllDocuments = function(collection, callback) {
 
 exports.findDocs = function(collection, matchHash, callback) {
   connect(function(db){
-    var cursor = db.collection(collection).find(matchHash);
+    var cursor = db.collection("config").find(matchHash);
     var ret = [];
     cursor.each(function(err, doc){
       if(doc != null)
