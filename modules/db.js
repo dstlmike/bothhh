04914cb6987b93b96bec6346c91e755dccd5393d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-13 22:57:17 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -209,8 +209,9 @@ var allDocs = db.collection(collection).find().toArray(function(err, docs) {
      */
 
   var uri = 'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-    var client = new MongoClient(uri); //'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
-    try {
+   // var client = new MongoClient(uri); //'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
+  var client = MongoClient.connect(uri);
+  try {
         // Connect to the MongoDB cluster
         await client.connect();
         // Make the appropriate DB calls
