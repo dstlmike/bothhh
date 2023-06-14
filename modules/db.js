22cdf8ec62cc8e9c100a67f1048cf8a4d5c63dc5
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-13 22:36:52 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -32,7 +32,7 @@ console.log('Results!!! ' + db.name);
 
 //run().catch(console.dir);
 //var client = MongoClient.connect(uri);
-//var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
 var uri = "mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 /*async function listCars() {
@@ -194,7 +194,7 @@ run().catch(console.dir);
 */
 
 //
-const {MongoClient} = require('mongodb');
+//const {MongoClient} = require('mongodb');
 //main().catch(console.error);
 async function main(){
 
