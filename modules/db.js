81a641f796de474f84d9f52fbad101ee63f2fc6d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-13 22:42:50 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -207,8 +207,8 @@ var allDocs = db.collection(collection).find().toArray(function(err, docs) {
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
 
-  var uri = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb';
-    var client = new MongoClient(uri);
+  var uri = 'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+    var client = new MongoClient('mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
     try {
         // Connect to the MongoDB cluster
         await client.connect();
