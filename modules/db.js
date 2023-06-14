eadbe805e19217ed553c38397e8948167f81baf7
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-13 22:59:09 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -210,7 +210,7 @@ var allDocs = db.collection(collection).find().toArray(function(err, docs) {
 
   var uri = 'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
    // var client = new MongoClient(uri); //'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
-  var client = MongoClient.connect(uri);
+  var client = MongoClient(uri);
   try {
         // Connect to the MongoDB cluster
         await client.connect();
