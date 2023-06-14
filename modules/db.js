5b3d2504c69c2c6286843497903cbb4deb8c943a
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-14 04:16:28 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -306,8 +306,8 @@ exports.getAllDocuments = function(collection, callback) {
   mongoDB.connect('mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, db) {
     if(err) throw err;
    // var sampledb3 = [];
-    var allDocs = db.collection(collection).find().toArray(function(err, results) {
-      callback(results);
+    var allDocs = db.collection(collection).find().toArray(function(err, docs) {
+      callback(docs);
       db.close();
     });
   });
@@ -317,8 +317,8 @@ exports.findDocs = function(collection, matchHash, callback) {
   connect(function(db){
     var cursor = db.collection("config").find(matchHash);
     var ret = [];
-    cursor.each(function(err, doc){
-      if(doc != null)
+    cursor.each(function(err, docs){
+      if(docs != null)
         ret.push(doc);
       else
         callback(ret);
