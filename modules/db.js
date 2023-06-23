dda873236697713ca57b580a8d10bcaf9162428e
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 21:15:48 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -1,13 +1,27 @@
 var mongoDB     = require('mongodb').MongoClient;
 var db = require('mongodb').Db
 var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var url = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 //var connection_string = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 var mongodbOptions = {
 useNewUrlParser: true,
 useUnifiedTopology: true
 }
 var MongoClient = require('mongodb').MongoClient;
-
+const client = new MongoClient(url, { useUnifiedTopology: true }); // { useUnifiedTopology: true } removes connection warnings;
+const dbName = "test"; 
+client 
+  .connect() 
+  .then( 
+    client => 
+      client 
+      .db(dbName) 
+      .listCollections() 
+      .toArray() // Returns a promise that will resolve to the list of the collections 
+      ) 
+      .then(cols => console.log("Collections", cols)) 
+      .finally(() => client.close());
 
 //main().catch(console.error);
 //getAllDocuments();
