ea28394ab61cc466ee4321011f4c476621219e20
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 21:51:32 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -11,7 +11,7 @@ useUnifiedTopology: true
 var MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url); //, { useUnifiedTopology: true }); // { useUnifiedTopology: true } removes connection warnings;
 const dbName = "test"; 
-client.connect(url).then(client => 
+client.connect().then(client => 
       client.db(dbName).listCollections().toArray()).then(cols => console.log("Collections", cols)) // Returns a promise that will resolve to the list of the collections 
      // )
      // .then(cols => console.log("Collections", cols));
