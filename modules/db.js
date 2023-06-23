2f6bb6662d1c9279b4b2871fe966760b5791107d
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 23:15:45 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -231,7 +231,7 @@ var allDocs = db.collection(collection).find().toArray(function(err, docs) {
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
 
-  var uri = 'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb3?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+  var uri = 'mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
    var client = new MongoClient(uri); //'mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority');
  // var client = MongoClient(uri);
   try {
