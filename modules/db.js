90860cc94f92a48ac4186fae464825c864431c91
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 23:34:27 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -11,9 +11,9 @@ useUnifiedTopology: true
 }
 main().catch(console.error);
        async function getCollectionList(client) { console.log('start ' + client);
-              var url = "mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+              var uri = "mongodb://alexbot:308boonave@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-                                                 var client = new MongoClient(url)
+                                                 var client = new MongoClient(uri)
                                                             collectionList = await client.db("sampledb").getCollectionNames(); 
                                                             console.log("Collections: " + collectionList); 
                                                              }
