05897b32d5bcb9138e6a861862630348e4fd7a8b
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-06-22 20:03:39 -0400

    Update db.js

--- modules/db.js
+++ modules/db.js
@@ -9,7 +9,7 @@ useUnifiedTopology: true
 var MongoClient = require('mongodb').MongoClient;
 
 
-main().catch(console.error);
+//main().catch(console.error);
 //getAllDocuments();
 /*function getAllDocuments(res) {
 mongoDB.connect("mongodb://boonbot:boonbot@cluster0-shard-00-00.esmha.mongodb.net:27017,cluster0-shard-00-01.esmha.mongodb.net:27017,cluster0-shard-00-02.esmha.mongodb.net:27017/sampledb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", function(err, db) {
